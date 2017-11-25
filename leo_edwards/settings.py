2a1ffcf6eede84220991cffad7061c5fa95da2ba
Leo Edwards <thejpe100@gmail.com>
2017-11-25 20:46:46 +0000

    First Deploy

--- leo_edwards/settings.py
+++ leo_edwards/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
-from leo_edwards import config
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -22,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.SECRET_KEY
+SECRET_KEY = 'l384t7iqa%!%1=-3qy$4g4$77k#yvmy581kq5e5s!!6di3-0$v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
